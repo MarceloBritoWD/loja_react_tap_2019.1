a136711dad0d5c90e7e0fed5e117a8a51ea3770d
Marcelo brito <marcelobritowd95@gmail.com>
2019-05-08 21:49:29 -0300

    Updates

---
+++ precache-manifest.905ffcc685facf404b5403db119d8a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16c02b4ab38ace5a1d71",
+    "url": "/loja_react_tap_2019.1/static/js/runtime~main.9f59f3a3.js"
+  },
+  {
+    "revision": "719720b229385835612b",
+    "url": "/loja_react_tap_2019.1/static/js/main.29ec6909.chunk.js"
+  },
+  {
+    "revision": "30d7d5eb4768e2b5b7de",
+    "url": "/loja_react_tap_2019.1/static/js/2.ec93695b.chunk.js"
+  },
+  {
+    "revision": "719720b229385835612b",
+    "url": "/loja_react_tap_2019.1/static/css/main.fe48edf5.chunk.css"
+  },
+  {
+    "revision": "30d7d5eb4768e2b5b7de",
+    "url": "/loja_react_tap_2019.1/static/css/2.c52e5424.chunk.css"
+  },
+  {
+    "revision": "6569bb2523b306655e0c58d226baf92e",
+    "url": "/loja_react_tap_2019.1/index.html"
+  }
+];
